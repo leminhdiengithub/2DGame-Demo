a4e6648b053f8f20acdd2ec570dc5d038917f228
Lê Minh Diện <142578015+leminhdiengithub@users.noreply.github.com>
2024-08-26 19:08:16 +0700

    Collision Change

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -10,11 +10,30 @@ bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& col
 {
     if(AABB(colA.collider, colB.collider))
     {
-        std::cout << colA.tag << " hit " << colB.tag << std::endl;
+        //A_Right-B_Left
+        if (colA.collider.x + colA.collider.w >= colB.collider.x)
+        {
+            
+            return true;
+        }
+        //A_Left B_Right
+        if (colB.collider.x + colB.collider.w >= colA.collider.x)
+        {
+            return true;
+        }
+        //A_Bottom B_Top
+        if (colA.collider.y + colA.collider.h >= colB.collider.y)
+        {
+            return true;
+        }
+        //A_Top B_Bottom
+        if (colB.collider.y + colB.collider.h >= colA.collider.y)
+        {
+            return true;
+        }
         return true;
     }else 
     {
         return false;
     }
-    
 }
