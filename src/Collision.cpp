a561ecdb7d9beee893299a6b02b67f0c2a0be262
Lê Minh Diện <142578015+leminhdiengithub@users.noreply.github.com>
2024-08-28 14:18:54 +0700

    Change collision

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -12,32 +12,13 @@ bool Collision::AABB(const SDL_Rect & A, const SDL_Rect & B)
 
 bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& colB)
 {
-    if(AABB(colA.collider, colB.collider))
+    if (AABB(colA.collider, colB.collider))
     {
-        //A_Right-B_Left
-        if (colA.collider.x + colA.collider.w >= colB.collider.x)
-        {
-            
-            return true;
-        }
-        //A_Left B_Right
-        if (colB.collider.x + colB.collider.w >= colA.collider.x)
-        {
-            return true;
-        }
-        //A_Bottom B_Top
-        if (colA.collider.y + colA.collider.h >= colB.collider.y)
-        {
-            return true;
-        }
-        //A_Top B_Bottom
-        if (colB.collider.y + colB.collider.h >= colA.collider.y)
-        {
-            return true;
-        }
+        std::cout << colA.tag << " hit " << colB.tag << std::endl;
         return true;
-    }else 
+    }
+    else
     {
         return false;
     }
-}
+} // sliding colision
