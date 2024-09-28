a506d6d9782535b399e81a93e203bddedc897e02
Lê Minh Diện <142578015+leminhdiengithub@users.noreply.github.com>
2024-09-28 18:42:54 +0700

    Add new ProjectSound

--- include/ECS/KeyboardController.hpp
+++ include/ECS/KeyboardController.hpp
@@ -10,6 +10,7 @@ class KeyboardController : public Component
 public: 
     TransformComponent *transform;
 	SpriteComponent *sprite;
+	AudioComponent *song;
     const Uint8* keystates = SDL_GetKeyboardState(NULL);
 
 
@@ -17,6 +18,8 @@ public:
     {
         transform = &entity->getComponent<TransformComponent>();
 		sprite = &entity->getComponent<SpriteComponent>();
+		//song = &entity->getComponent<AudioComponent>();
+		//song = &entity->addComponent<AudioComponent>("res/sounds/Projectile_sound.mp3");
     }
 
     void update() override {
@@ -51,6 +54,7 @@ public:
 		if (keystates[SDL_SCANCODE_J])
 		{
 			Game::assets->CreateProjectile(Vector2D(transform->position.x * 0.5, transform->position.y *0.5),Vector2D(2,0) ,200, 2, "projectile");
+			//song->playMusic();
 		}
 	}
 };
