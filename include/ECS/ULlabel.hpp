636d8ee4e29a9e03280b36ddc2d78d117f431407
Lê Minh Diện <142578015+leminhdiengithub@users.noreply.github.com>
2025-06-26 12:57:32 +0700

    Create new menu

--- include/ECS/ULlabel.hpp
+++ include/ECS/ULlabel.hpp
@@ -1,35 +1,54 @@
-class ULlabel : public Component
-{
+#pragma once
+
+#include "ECS/ECS.hpp"
+#include "../AssetManager.hpp"
+#include "../Game.hpp"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+
+class ULlabel : public Component {
 public:
     ULlabel(int xpos, int ypos, std::string text, std::string font, SDL_Color colour, int fontSize = 24)
         : labelText(text), labelFont(font), textColour(colour), fontSize(fontSize)
     {
         position.x = xpos;
         position.y = ypos;
-        setLabelText(labelText);
+        updateTexture();
     }
 
     virtual ~ULlabel() {
         if (labelTexture) SDL_DestroyTexture(labelTexture);
     }
 
-    virtual void setLabelText(const std::string& text)
-    {
+    virtual void draw() override {
+        SDL_RenderCopy(Game::renderer, labelTexture, nullptr, &position);
+    }
+
+    void setLabelText(const std::string& text) {
         labelText = text;
-        SDL_Surface* surf = TTF_RenderText_Blended(Game::assets->GetFont(labelFont), text.c_str(), textColour);
+        updateTexture();
+    }
+
+    std::string getText() const { return labelText; }
+
+    SDL_Rect getRect() const { return position; }
+
+    void setPosition(int x, int y) {
+        position.x = x;
+        position.y = y;
+    }
+
+protected:
+    void updateTexture() {
+        SDL_Surface* surf = TTF_RenderText_Blended(Game::assets->GetFont(labelFont), labelText.c_str(), textColour);
         if (labelTexture) SDL_DestroyTexture(labelTexture);
         labelTexture = SDL_CreateTextureFromSurface(Game::renderer, surf);
         SDL_FreeSurface(surf);
 
         SDL_QueryTexture(labelTexture, nullptr, nullptr, &position.w, &position.h);
     }
-
-    virtual void draw() override {
-        SDL_RenderCopy(Game::renderer, labelTexture, nullptr, &position);
-    }
-
-protected:
-    SDL_Rect& getPosition() { return position; }
 
 protected:
     SDL_Rect position;
