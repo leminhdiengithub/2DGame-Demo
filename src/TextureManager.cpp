9d41c7f53f3a1f80c03cc0e77d00288e769251c0
unknown <leminhdien20043333@gmail.com>
2025-07-07 18:23:09 +0700

    ...

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -12,5 +12,5 @@ SDL_Texture* TextureManager::loadTexture(const char* texture)
 
 void TextureManager::Draw(SDL_Texture* tex, SDL_Rect src, SDL_Rect dest, SDL_RendererFlip flip)
 {
-    SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, NULL, NULL, flip);
+    SDL_RenderCopyEx(Game::renderer, tex, &src, &dest, 0.0, NULL, flip);
 }
